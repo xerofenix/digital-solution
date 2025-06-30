d8e0f7f7dbf46714290f49a7cc6057a6d4243b99
xerofenix <xerofe@tuta.io>
2025-06-30 14:45:39 +0530

    add main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+/**
+ * This file is the entry point for the React app, it sets up the root
+ * element and renders the App component to the DOM.
+ *
+ * It is included in `src/index.html`.
+ */
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
