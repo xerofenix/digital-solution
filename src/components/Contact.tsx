03515a30dce09c6afd88f52ce81abfabc0e84cc6
xerofenix <xerofe@tuta.io>
2025-07-05 20:48:00 +0530

    update contact.tsx for misbah start working

---
+++ src/components/Contact.tsx
@@ -0,0 +1,10 @@
+import './Contact.css'
+
+export default function Contact() {
+    return (
+        <div>
+            <h1 className='contact-delete'>This is contact page and you have to work upon it</h1>
+            <h1 className='contact-delete'>Edit this and start working on it</h1>
+        </div>
+    );
+}
