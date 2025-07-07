cbb38481541102547f50ae897282088797bd9ec5
xerofenix <xerofe@tuta.io>
2025-07-07 15:53:29 +0530

    add blank services page

---
+++ src/components/Services.tsx
@@ -0,0 +1,9 @@
+import './Services.css'
+
+export default function Services() {
+    return (
+        <div>
+            <h1 className='service'>This is service page has to be developed</h1>
+        </div>
+    );
+}
