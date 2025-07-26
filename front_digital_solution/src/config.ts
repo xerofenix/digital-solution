8f454b0c62ad643b8d681c1661d558149863a0da
xerofenix <xerofe@tuta.io>
2025-07-26 22:02:46 +0530

    Changing port to fetch data

--- front_digital_solution/src/config.ts
+++ front_digital_solution/src/config.ts
@@ -1,4 +1,4 @@
 // src/config.ts
-const HOME_URL = import.meta.env?.REACT_APP_HOME_URL || 'http://localhost:8000';
+const HOME_URL = import.meta.env?.REACT_APP_HOME_URL || "http://localhost:4000";
 // console.log('Config HOME_URL:', HOME_URL);
 export { HOME_URL };
